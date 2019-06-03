84d5ce0d2f1fb9271a6545f756a24ea943437529
vnicius <vinicius.matheus252@gmail.com>
2019-06-03 10:04:45 -0300

    Remove Instance Reader constructor

--- src/InstanceReader.py
+++ src/InstanceReader.py
@@ -5,26 +5,15 @@ from src.Instance import Instance
 
 class InstanceReader():
     """Read the file of instances
-
-    Attribute:
-        instance_path {str} -- path of the file with the instances
     """
 
-    def __init__(self, instance_path):
-        """Constructor
-
-        Arguments:
-            instance_path {str} -- path of the file with the instances
-        """
-        self.instance_path = instance_path
-
-    def get_instance(self):
+    def get_instance(self, instance_path):
         instance = None
         name = ""
         dimension = ""
         data = []
 
-        with open(self.instance_path, 'r', encoding='utf-8') as file:
+        with open(instance_path, 'r', encoding='utf-8') as file:
             name = file.readline().split(' ')[-1]
             dimension = int(file.readline().split(' ')[-1])
             data = np.zeros(shape=(dimension, dimension))
